929ddd228b62fe2621e41588fa836424a09b418b
John Wang <john@johntwang.com>
2011-01-30 15:25:13 -1000

    refinerycms generic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aloharb::Application.config.secret_token = '3dab592dd75ca022b991af1f461f4f9f3b219c9dba518a2281fb4ae621d6419eff5000800732b225084556f0683c4016f58fdf018ed538082c565399d98d6c3c'
